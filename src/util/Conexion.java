dcd1bef0787d0969f32b0d1d4c695cbcfdf2428f
User <User@DESKTOP-ONNSLHR>
2019-12-10 02:51:55 +0100

    subida proyect

---
+++ src/util/Conexion.java
@@ -0,0 +1,27 @@
+package util;
+import java.sql.*;
+
+public class Conexion {
+	
+	Connection conex = null;
+
+	public Conexion() {
+		
+		
+	}
+	
+	public Connection dameConexion() {
+		
+			try {
+				conex=DriverManager.getConnection("jdbc:mysql://localhost:3306/vuelos?serverTimezone=UTC", "root", "");
+				System.out.println("Conexion correcta");
+			}catch(Exception e){
+				System.out.println("error de conexion");
+				e.printStackTrace();
+			}
+			
+			return conex;
+	}
+
+
+}
